8da7222b0cfcfbb90ed52100cd7a28b80c9710db
Fiama Monte <fiacarolina@live.com>
2022-07-18 17:58:34 -0300

    2dA ENTREGA

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () =>{
+    return
+    <header>
+        <h1>
+            VITTO STORE
+        </h1>
+        <nav>
+            <p> Enlace 1</p>
+            <p> Enlace 2</p>
+            <p> Enlace 3</p>
+        </nav>
+    </header>
+}
+export default NavBar
